1df6feb0c88de9e0c928f635c9bd3485edde5c54
selt0 <chewedgum12@gmail.com>
2019-12-28 20:42:42 -0600

    Complete ch4 ex 2

--- Ch4ex.js
+++ Ch4ex.js
@@ -28,9 +28,60 @@ function range(start, end, step = 1) {
 }
 
 function sum(array) {
+  let total = 0;
   for (let i = 0; i < array.length; i++) {
-    let total = 0;
     total += array[i];
   }
   return total;
+}
+
+//clever solution
+function range(start, end, step = start < end ? 1 : -1) {
+  let array = [];
+
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) array.push(i);
+  } else {
+    for (let i = start; i >= end; i += step) array.push(i);
+  }
+  return array;
+}
+
+function sum(array) {
+  let total = 0;
+  for (let value of array) {
+    total += value;
+  }
+  return total;
+}
+
+//Reversing an array
+
+/*Arrays have a reverse method that changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method.
+
+Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one runs faster?
+
+// Your code here.
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
+*/
+
+function reverseArray(array) {
+  let reverse = [];
+  for (let value of array) reverse.unshift(value);
+  return reverse;
+}
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let temp = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = temp;
+  }
+  return array;
 }
